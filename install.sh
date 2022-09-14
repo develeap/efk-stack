a4bb62b28c32c05f077f9c0f86d0b26ffca9a04d
Gilad Neiger <gilad.neiger@develeap.com>
2022-09-14 12:10:41 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--namespace)
+      NAMESPACE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--password)
+      KIBANA_PWD="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
+
+if [ -z "${NAMESPACE}"] || [ -z "${KIBANA_PWD}" ]; then
+    echo "USAGE: ./install.sh -n NAMESPACE -p KIBANA_PWD"
+    exit 1
+fi
+
+/usr/local/bin/helm repo add elastic https://helm.elastic.co
+/usr/local/bin/helm repo update
+kubectl create ns ${NAMESPACE}
+/usr/local/bin/helm upgrade --install elastic-operator elastic/eck-operator -n ${NAMESPACE}
+/usr/local/bin/helm upgrade --install eck-stack eck-stack/ -n ${NAMESPACE} --set kibana.secret.pwd=${KIBANA_PWD}
---
+++ uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--namespace)
+      NAMESPACE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
+
+/usr/local/bin/helm delete eck-stack -n ${NAMESPACE}
+/usr/local/bin/helm delete elastic-operator -n ${NAMESPACE}
+kubectl delete ns ${NAMESPACE}
